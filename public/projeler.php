a0ee246341fd87128fcf59c690ffe8a605dfa337
Mahmut Yüksel Mert <mahmutyukselmert@gmail.com>
2025-09-19 20:41:03 +0300

    yeni revizyonlar

---
+++ public/projeler.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antek Makina - Mühendislik ve Makina Sanayi</title>
+    <?php include 'include/head.html'; ?>
+
+    <!-- Swiper CSS - Sadece Proje Detay -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+
+<body class="sub-page linear-gradient-parlement">
+    <?php include 'include/header.html'; ?>
+
+    <main class="sub-page-main">
+
+        <section class="page-banner">
+            <div class="background-wrapper">
+                <img src="./assets/images/proje-detay-banner.webp" alt="Banner">
+            </div>
+            <div class="page-banner-content">
+                <div class="container">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-lg-11 col-11">
+                            <h1>PROJELERİMİZ</h1>
+                            <p class="subtitle">Projelerimize göz atabilirsiniz.</p>
+                            <div class="row col-lg-12">
+                                <p class="contact-us-banner-paragraph">
+                                    Konum, ve kategori ile projeleri filtreleyebilirsiniz.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="project-filter-section">
+            <div class="container py-4 filter-box">
+                <div class="row">
+                    <div class="col-12 d-lg-flex align-items-center gap-3">
+                        <label class="form-label mb-0 fw-bold text-muted">FİLTRELE:</label>
+
+                        <div class="flex-grow-1">
+                            <select class="form-select mb-2 mb-lg-0">
+                                <option selected>ALTYAPI</option>
+                                <option value="1">Seçenek 1</option>
+                                <option value="2">Seçenek 2</option>
+                            </select>
+                        </div>
+
+                        <div class="flex-grow-1">
+                            <select class="form-select mb-2 mb-lg-0">
+                                <option selected>KONUM</option>
+                                <option value="1">Seçenek 1</option>
+                                <option value="2">Seçenek 2</option>
+                            </select>
+                        </div>
+
+                        <div class="flex-grow-1">
+                            <select class="form-select mb-2 mb-lg-0">
+                                <option selected>TÜM ALTYAPILAR</option>
+                                <option value="1">Seçenek 1</option>
+                                <option value="2">Seçenek 2</option>
+                            </select>
+                        </div>
+
+                        <div class="ms-auto">
+                            <button type="button" class="btn btn-primary rounded-0 mb-2 mb-lg-0">FİLTREYİ TEMİZLE</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="projects-list-sections">
+            <div class="container px-0">
+                <div class="flex-row">
+                    
+                    <!-- Project Item Start -->
+                    <div class="project-item-row">
+                        <a href="proje-detay.php" class="projects-link">
+                            <div class="image-container">
+                                <img src="https://www.lycopodium.com/static/7bd17a69c84c28fdb5a2e7080d345d71/d8255/file5-7a-min.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="text-container">
+                                <div class="text-wrapper">
+                                    <span class="title">Engineering and Systematic Enhancements</span>
+                                    <span class="sector">Infrastructure / Project Management / Rail Infrastructure Management</span>
+                                    <span class="location notranslate">Australia</span>
+                                    <p class="summary-text">Pacific National (PN) has tasked Lycopodium with leading them towards the implementation of various innovative solutions and improvements. The process has been fully funded by PN and ...</p>
+                                </div>
+                                <div class="button">
+                                    <button href="iletisim.php" class="btn btn-outline-primary text-white text-decoration-none rounded-0 px-4 mt-4">
+                                        <span>BİZİMLE İLETİŞİME GEÇİN</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor" class="bi bi-arrow-right-short">
+                                            <path d="M571.3 267.3c6.2-6.2 6.2-16.4 0-22.6l-144-144c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L521.4 240 16 240c-8.8 0-16 7.2-16 16s7.2 16 16 16l505.4 0-116.7 116.7c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0l144-144z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Project Item End -->
+
+                    <div class="project-item-row">
+                        <a href="proje-detay.php" class="projects-link">
+                            <div class="image-container">
+                                <img src="https://www.lycopodium.com/static/7bd17a69c84c28fdb5a2e7080d345d71/d8255/file5-7a-min.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="text-container">
+                                <div class="text-wrapper">
+                                    <span class="title">Engineering and Systematic Enhancements</span>
+                                    <span class="sector">Infrastructure / Project Management / Rail Infrastructure Management</span>
+                                    <span class="location notranslate">Australia</span>
+                                    <p class="summary-text">Pacific National (PN) has tasked Lycopodium with leading them towards the implementation of various innovative solutions and improvements. The process has been fully funded by PN and ...</p>
+                                </div>
+                                <div class="button">
+                                    <button href="iletisim.php" class="btn btn-outline-primary text-white text-decoration-none rounded-0 px-4 mt-4">
+                                        <span>BİZİMLE İLETİŞİME GEÇİN</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor" class="bi bi-arrow-right-short">
+                                            <path d="M571.3 267.3c6.2-6.2 6.2-16.4 0-22.6l-144-144c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L521.4 240 16 240c-8.8 0-16 7.2-16 16s7.2 16 16 16l505.4 0-116.7 116.7c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0l144-144z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="contact-us-we">
+            <div class="container">
+
+                <div class="col-12 col-lg-12">
+                    <div class="contact-info-card">
+                        <h2>BİZİMLE İLETİŞİME GEÇİN</h2>
+                        <p class="subheading">Sorunuz mu var? İletişime geçin.</p>
+                        <div class="col-lg-6 col-12 text-center">
+                            <p>Küresel mühendislik, inşaat ve proje yönetimi uzmanlığımızı ticari avantajınıza nasıl dönüştürebileceğinizi öğrenin.</p>
+                        </div>
+                        <a href="iletisim.php" class="btn btn-outline-white text-decoration-none rounded-0 px-4 mt-4">
+                            <span>BİZİMLE İLETİŞİME GEÇİN</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor" class="bi bi-arrow-right-short">
+                                <path d="M571.3 267.3c6.2-6.2 6.2-16.4 0-22.6l-144-144c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L521.4 240 16 240c-8.8 0-16 7.2-16 16s7.2 16 16 16l505.4 0-116.7 116.7c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0l144-144z"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    </main>
+
+    <?php include 'include/footer.html'; ?>
+
+    <!-- Swiper JS - Sadece Proje Detay -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script>
+        const swiper = new Swiper('.swiper', {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            loop: true,
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+            speed: 500,
+            effect: 'coverflow',
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    </script>
+
+</body>
+
+</html>
